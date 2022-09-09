1091bb41c9b139eb004d771c3adcc6d93a6a4194
Simfara R <simfararanjit@gmail.com>
2022-09-08 20:10:09 -0500

    Challenge 1 requirement2

---
+++ Challenge1/Assets/Challenge 1/Scripts/LoseConditionX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this to plane
+public class LoseConditionX : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player.transform.position.y is above 80 
+        //or below -51 and if so, display game over text.
+
+        if (transform.position.y > 80 || transform.position.y < -51)
+        {
+            ScoreManagerX.gameOver = true;
+        }
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/PlaneEnterTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this to trigger zone
+public class PlaneEnterTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Plane") && !triggered)
+        {
+            triggered = true;
+            ScoreManagerX.score++;
+        }
+    }
+}

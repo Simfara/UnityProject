2334323d89f367149972f502ead4a4a8333eee5d
Simfara R <simfararanjit@gmail.com>
2022-09-20 21:48:16 -0500

    PLZ WORK

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,24 +8,47 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public float jumpForce;
-    public bool isOnGround = true;
+    public float gravityModifier;
     public ForceMode forceMode;
+
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+   
     // Start is called before the first frame update
     void Start()
     {
         //set a refrence to our rigidbody component
 
         rb = GetComponent<Rigidbody>();
-        
+        forceMode = ForceMode.Impulse;
+
+        //Modify gravity
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
         }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GameOver!");
+            gameOver = true;
+        }
     }
 }

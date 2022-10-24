c95a4dc6c85c5cba3c37c7640968bcb6a4cf19ca
Simfara R <simfararanjit@gmail.com>
2022-10-23 20:43:01 -0500

    Follow Along Complete

---
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRayCast.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRayCast : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    public float hitForce = 10f;
+     void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hitInfo;
+        //if we hit somth with the ray
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+           
+
+            //Get the targetscript off the hit obkect
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            //if target script found make the target take damage
+            if (target !=null)
+            {
+                target.TakeDamage(damage);
+            }
+            //If the shot hits a rigidbody, apply a force
+            if (hitInfo.rigidbody !=null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}

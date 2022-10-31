867dbb8a2d5a83494d1a763ff48bcf9730380c5c
Simfara R <simfararanjit@gmail.com>
2022-10-31 00:26:37 -0500

    TriggerScript

---
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/EndTriggerZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTriggerZone : MonoBehaviour
+{
+    private bool triggered = false;
+    public Text textbox;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone") && !triggered)
+        {
+            triggered = true;
+            textbox.text = "You Win!";
+        }
+    }
+}
